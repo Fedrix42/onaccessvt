d16c2cc1886b76833e27bc2691947db5531d7ba9
Fedrix <144663254+Fedrix42@users.noreply.github.com>
2023-09-20 14:44:27 +0200

    Better crontab setting
    Now the script can be executed multiple times without adding redundant crontab records(It checks if there is already a reconrd containing a keyword).
    Also added the error message if the script is not runned as root.

--- onaccessvt/setup.sh
+++ onaccessvt/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-user='ubuntu'
+user='fedrix'
+
+#Check if script is runned with sudo
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 #---Dependencies
 sudo -u $user pip install vt-py
@@ -8,7 +14,7 @@ sudo apt install python3-tk
 
 #Compile monitor
 mkdir ./monitor/bin/
-gcc ./monitor/src/main.c ./monitor/src/mark.c ./monitor/src/logger.c -o ./monitor/bin/onaccessvt_monitor
+gcc ./monitor/src/main.c ./monitor/src/mark.c ./monitor/src/logger.c ./monitor/src/set_config.c -o ./monitor/bin/onaccessvt_monitor
 chmod +x ./monitor/bin/onaccessvt_monitor
 
 #Copy current folder into /opt
@@ -21,14 +27,19 @@ sudo mkdir /var/log/onaccessvt/interface
 sudo mkdir /var/log/onaccessvt/monitor
 sudo chown -R $user:$user /var/log/onaccessvt/
 
-#Adding crontab for onaccessvt interface to user
+#Adding crontab for onaccessvt interface to user(only if it was not added before)
 sudo -u $user crontab -l > mycron
-echo "@reboot /opt/onaccessvt/onaccessvt_interface.start" >> mycron
-sudo -u $user crontab mycron
+if ! [[ $(cat mycron | grep onaccessvt_interface) ]]; then
+	echo "@reboot /opt/onaccessvt/onaccessvt_interface.start" >> mycron
+	sudo -u $user crontab mycron
+fi
 rm mycron
 
-#Adding crontab for onaccessvt monitor to root user
+
+#Adding crontab for onaccessvt monitor to root user(only if it was not added before)
 crontab -l > mycron_root
-echo "@reboot sleep 30s && /opt/onaccessvt/monitor/bin/onaccessvt_monitor $(echo $monitor_args)" >> #mycron_root
-crontab mycron_root
+if ! [[ $(cat mycron_root | grep onaccessvt_monitor) ]]; then
+	echo "@reboot sleep 30s && /opt/onaccessvt/monitor/bin/onaccessvt_monitor $(echo $monitor_args)" >> mycron_root
+	crontab mycron_root
+fi
 rm mycron_root
