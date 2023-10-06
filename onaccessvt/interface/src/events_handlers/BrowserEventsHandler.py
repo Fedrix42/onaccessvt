d3a1d77e904fa466c64057b84fa5725218b480db
Fedrix <144663254+Fedrix42@users.noreply.github.com>
2023-10-06 18:13:26 +0200

    Better file naming and added comments

--- onaccessvt/interface/src/events_handlers/BrowserEventsHandler.py
+++ onaccessvt/interface/src/events_handlers/BrowserEventsHandler.py
@@ -4,11 +4,11 @@ import tkinter.messagebox
 from additionals.messages import infos
 from additionals.Logger import Logger
 from data_types.EventData import EventData
-from events_handlers.EventsHandler import EventsHandler
+from events_handlers.SafeEventsHandler import SafeEventsHandler
 from requests_handlers.RequestsHandler import RequestsHandler
 
 
-class BrowserEventsHandler(EventsHandler):
+class BrowserEventsHandler(SafeEventsHandler):
 
     def __init__(self, rhandler: RequestsHandler, logger : Logger):
         super().__init__(rhandler, logger)
@@ -17,8 +17,13 @@ class BrowserEventsHandler(EventsHandler):
         super().call_request(event_data)
 
     def handle_event(self, event_data: EventData) -> None:
+        """Checks whenever the file is being downloaded by browser 
+        and send a alert to the user to scan it after the download if finished"""
+
         if event_data.__getattribute__('procinfo').__getattribute__("isABrowser"):
-            time.sleep(1)
+            time.sleep(1) 
+            """This delay is helpful because some browsers create a .part file 
+            and some time passes between this code executed and the creation of the .part file"""
             if self.is_download_in_process(event_data):
                 if tkinter.messagebox.askyesno(infos.DIAL_BOX_TITLE, infos.DIAL_BOX_BODY.format(file=event_data.__getattribute__("entryName"))):
                     super().handle_event(event_data)
@@ -28,9 +33,13 @@ class BrowserEventsHandler(EventsHandler):
             super().handle_event(event_data)
 
     def is_download_in_process(self, event_data: EventData) -> bool:
+        """Check if file is being downloaded by different browsers based on their behaviour.
+        I should find a way to do this that is the same for all browsers."""
         return self.firefox_check(event_data) or self.chrome_check(event_data)
 
     def firefox_check(self, event_data: EventData):
+        """Check if the file is being downloaded by a firefox browser.
+        Searches for a .part file on the same directory of the event file"""
         filename = event_data.__getattribute__("entryName")
         entriesInCWD = [f for f in os.listdir(event_data.__getattribute__('directoryPath'))]
         for entry in entriesInCWD:
@@ -39,6 +48,7 @@ class BrowserEventsHandler(EventsHandler):
         return False
 
     def chrome_check(self, event_data : EventData):
+        """Check if the file is being downloaded by a chrome browser."""
         if event_data.__getattribute__("entryName").endswith("crdownload"):
             return True
         return False
--- onaccessvt/interface/src/events_handlers/EventsHandler.py
+++ onaccessvt/interface/src/events_handlers/EventsHandler.py
@@ -1,71 +1,21 @@
+from abc import ABC, abstractmethod
 from data_types.EventData import EventData
-from events_handlers.ABCEventsHandler import ABCEventsHandler
-from requests_handlers.VTRequestsHandler import RequestsHandler
-from notifications.handlers.NTSendNotificationsHandler import NTSendNotificationsHandler
-from notifications.Notification import Notification
-from additionals.Logger import Logger
-from data_types.UrgencyType import UrgencyType
-from additionals.messages import errors
 
 
-class EventsHandler(ABCEventsHandler):
-    def __init__(self, requestHandler: RequestsHandler, logger : Logger) -> None:
-        self.requestHandler = requestHandler
-        self.notify_handler = NTSendNotificationsHandler()
-        self.logger = logger
-        self.verbose = False
+class EventsHandler(ABC):
+    """Manage events to verify when and how files need to be scanned"""
+    @abstractmethod
+    def handle_event(
+        self, event_data: EventData
+    ):  
+        pass
 
-    def handle_event(self, event_data: EventData) -> None:
-        self.put_out_data(event_data)
-        self.call_request(event_data)
+    @abstractmethod
+    def put_out_data(self, event_data: EventData): 
+        """Sends data to appropiate components to log them or to inform the user"""
+        pass
 
-    def put_out_data(self, event_data: EventData):
-        self.logger.log(event_data.to_log())
-        if self.verbose:
-            self.notify_handler.notify(
-                Notification(
-                    event_data.format_notification_title(),
-                    event_data.format_notification_body(),
-                    UrgencyType.NORMAL,
-                )
-            )
-
-    def call_request(self, file_data: EventData) -> None:
-        try:
-            result = self.requestHandler.handle_request(file_data)
-            self.logger.log(
-                result.format_notification_title()
-                + "\n"
-                + result.format_notification_body()
-            )
-            if self.verbose or self.isResultPotentiallyHarmful(result):
-                self.notify_handler.notify(
-                    Notification(
-                        result.format_notification_title(),
-                        result.format_notification_body(),
-                        UrgencyType.NORMAL,
-                    )
-                )
-        except Exception as e:
-            #raise e #debug
-            self.logger.log(
-                errors.SCAN.format(file=file_data.__getattribute__("entryName"), e=e)
-            )
-            self.notify_handler.notify(
-                Notification(
-                    errors.SCAN_TITLE,
-                    errors.SCAN.format(
-                        file=file_data.__getattribute__("entryName"), e=e
-                    ),
-                    UrgencyType.CRITICAL,
-                )
-            )
-
-    def isResultPotentiallyHarmful(self, result) -> bool:
-        if (
-            int(result.__getattribute__("vtobj").last_analysis_stats["malicious"]) > 0
-            or int(result.__getattribute__("vtobj").total_votes["malicious"]) > 0
-        ):
-            return True
-        return False
-
+    @abstractmethod
+    def call_request(self, event_data: EventData):  
+        """Call the component which manage the requests, log the results and inform the user of it"""
+        pass
